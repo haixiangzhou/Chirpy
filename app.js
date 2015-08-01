efd26899f67735d0e7c91223b94f43fa7c0f6bcf
haixiangzhou <d.zhouhx@gmail.com>
2015-08-01 15:48:24 -0400

    finish up the basic front end
    finish up the basic front end

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-var app = angular.module('chirpyApp', []);
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
 
-app.controller('mainController', function(){});
+app.use(express.static(__dirname + '/public'));     //serve static assets
+app.get('*', function(req, res) {
+    res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
+
+app.listen(port, function() {
+    console.log('Listening on ' + port);
+});
